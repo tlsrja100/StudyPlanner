a08074313373cc42aa41c06a266cd3c7c7791997
egoing <egoing@naver.com>
2023-03-05 22:45:08 +0900

    server port 추가 (8080) database 기본 추가

--- src/main/java/com/studyplanner/studyplanner/controller/PlannerController.java
+++ src/main/java/com/studyplanner/studyplanner/controller/PlannerController.java
@@ -6,8 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PlannerController {
 
-    @GetMapping("/mainPlanner")
-    public void getMainPlanner() {
-
-    }
 }
