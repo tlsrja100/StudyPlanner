f9b77f62566e540e88c395c3286acbbf784c7fd2
egoing <egoing@naver.com>
2023-03-05 21:27:38 +0900

    start

---
+++ src/main/java/com/studyplanner/studyplanner/StudyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.studyplanner.studyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyplanner/studyplanner/StudyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyplanner.studyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
